535e5f05e042e3212b33f086450159c4e16816ba
SnehAl2o7 <snehalsinghe353@gmail.com>
2026-02-01 23:39:35 +0530

    today's questions

---
+++ Array-Basic/LeftRotate.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotate(vector<int>& arr, int k){
+    int n = arr.size();
+    k = k % n;
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());
+    reverse(arr.begin(), arr.end());
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    leftRotate(arr, k);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
